d92be558c0216ca240940c6d2a6a76cf15ef8fcd
Daniel Spencer <dms305@hku.hk>
2019-04-11 14:01:48 +0800

    Add function for reading ProteinPilot XML results to readers.py

--- readers.py
+++ readers.py
@@ -215,6 +215,56 @@ def read_peptide_summary(summary_file, condition=None):
                 else [_build_ppres(r) for r in reader if condition(r)])
 
 
+def read_proteinpilot_xml(filename):
+    """
+    Reads the full ProteinPilot search results in XML format.
+    Note that reading this file using an XML parser does not appear to be
+    straightforward due to errors related to NCNames.
+
+    Args:
+        filename (str): The path to the result XML file.
+
+    Returns:
+
+    """
+    res = []
+    with open(filename, 'r') as f:
+        hits, t = [], False
+        for line in f:
+            sx = re.findall('"([^"]*)"', line.rstrip())
+            if line.startswith('<SPECTRUM'):
+                queryid = sx[6]
+                pms = float(sx[4])
+                t = True
+            elif t:
+                if line.startswith('<MATCH'):
+                    sline = line.rstrip().split('=')
+                    for ii, prop in enumerate(sx):
+                        if sline[ii].endswith('charge'):
+                            ck = int(prop)  # charge
+                        if sline[ii].endswith('confidence'):
+                            conf = float(prop)  # confidence
+                        if sline[ii].endswith('seq'):
+                            pk = prop  # sequence
+                        if sline[ii].endswith('type'):
+                            nk = 'decoy' if int(prop) == 1 else 'normal'
+                        if sline[ii].endswith('score'):
+                            sk = float(prop)
+                    modj = []
+                elif line.startswith('<MOD_FEATURE'):
+                    j = int(sx[1])
+                    modj.append('%s(%s)@%d' % (sx[0], pk[j-1], j))
+                elif line.startswith('<TERM_MOD_FEATURE'):
+                    if not sx[0].startswith('No'):
+                        modj.insert(0, 'iTRAQ8plex@N-term')
+                elif line.startswith('</MATCH>'):
+                    hits.append((pk, ';'.join(modj), ck, pms, conf, sk, nk))
+                elif line.startswith('</SPECTRUM>'):
+                    res.append((queryid, hits))
+                    hits, t = [], False
+    return res
+
+
 def read_fasta_sequences(fasta_file):
     """
     Retrieves sequences from the input fasta_file.
