8e2f790ec16f5214355312f3c0654058ffe4c3d4
Daniel Spencer <dms305@hku.hk>
2019-08-23 10:07:54 +0800

    Update setup.py to install package

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from distutils.core import setup
+import os
 
 from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules=cythonize(["rPTMDetermine/binomial.pyx", "rPTMDetermine/ionscore.pyx"]),
-    include_dirs=[np.get_include()]
+    name="rPTMDetermine",
+    version="0.1",
+    packages=[
+        "rPTMDetermine",
+        "rPTMDetermine.readers"
+    ],
+    ext_modules=cythonize(
+        [
+            os.path.join("rPTMDetermine", "annotate.pyx"),
+            os.path.join("rPTMDetermine", "binomial.pyx"),
+            os.path.join("rPTMDetermine", "ionscore.pyx")
+        ],
+        compiler_directives={'language_level' : "3"}
+    ),
+    include_dirs=[np.get_include()],
+    package_data={
+        "rPTMDetermine": ["EnzymeRules.json"],
+        "rPTMDetermine.readers": ["unimod.xml"]
+    },
+    include_package_data=True
 )
