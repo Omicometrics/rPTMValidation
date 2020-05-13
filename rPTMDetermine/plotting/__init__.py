8a7ba6227ed6da293006f86d58229fc9e19bf0a9
Daniel Spencer <dms305@hku.hk>
2020-05-13 14:29:32 +0800

    Remove old version score plot functions from plotting init

--- rPTMDetermine/plotting/__init__.py
+++ rPTMDetermine/plotting/__init__.py
@@ -3,10 +3,6 @@ from .psms import (
     plot_psms
 )
 from .scores import (
-    plot_recovered_score_similarity,
-    plot_score_similarity,
-    plot_scores,
-    plot_similarity_score_dist,
     plot_site_probabilities,
     plot_validated_recovered_site_probabilities
 )
@@ -18,10 +14,6 @@ from .spectra import (
 __all__ = [
     'plot_psm',
     'plot_psms',
-    'plot_recovered_score_similarity',
-    'plot_score_similarity',
-    'plot_scores',
-    'plot_similarity_score_dist',
     'plot_site_probabilities',
     'plot_spectra',
     'plot_spectrum',
