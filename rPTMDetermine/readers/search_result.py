e8c8d5791ef148a785341b070495ab1524c1f984
Daniel Spencer <dms305@hku.hk>
2020-05-26 18:42:12 +0800

    Update SearchResult and derived classes for pickling
    Due to an unresolved issue in unpickling, the __getstate__ and __setstate__ methods of a frozen dataclass need to be overridden to enable pickling to work correctly.

--- rPTMDetermine/readers/search_result.py
+++ rPTMDetermine/readers/search_result.py
@@ -6,6 +6,7 @@ search identification.
 """
 import dataclasses
 import enum
+import itertools
 from typing import Optional, Tuple
 
 from pepfrag import ModSite
@@ -21,7 +22,7 @@ class PeptideType(enum.Enum):
 
 
 @dataclasses.dataclass(eq=True, frozen=True)
-class SearchResult():  # pylint: disable=too-few-public-methods
+class SearchResult:  # pylint: disable=too-few-public-methods
     """
     A data class to store information about an identification from a database
     search engine. All search engine readers should return a list of
@@ -40,3 +41,18 @@ class SearchResult():  # pylint: disable=too-few-public-methods
     rank: int
     pep_type: PeptideType
     theor_mz: Optional[float]
+
+    def __getstate__(self):
+        return {
+            slot: getattr(self, slot)
+            for slot in itertools.chain.from_iterable(
+                getattr(cls, '__slots__', tuple())
+                for cls in type(self).__mro__) if hasattr(self, slot)
+        }
+
+    def __setstate__(self, state):
+        # This is added as a workaround for pickling frozen dataclasses, whereby
+        # unpickling will try to use `setattr` on the frozen dataclass:
+        # https://bugs.python.org/issue36424
+        for slot, value in state.items():
+            object.__setattr__(self, slot, value)
